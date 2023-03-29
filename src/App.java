cfdb4b6b7da33cbeefb68c52f07a0b4330b02075
F1984898 Celso Rodrigues de Freitas Junior <celsojunior@bb.com.br>
2023-03-29 00:30:30 -0300

    Aula 1 - desafio 2

--- src/App.java
+++ src/App.java
@@ -11,7 +11,7 @@ public class App {
 
         // fazer uma conexão HTTP e buscar os top 250 séries
         String imdbKey = System.getenv("IMDB_API_KEY");
-        String url = "https://imdb-api.com/en/API/Top250TVs/" + imdbKey;
+        String url = "https://imdb-api.com/en/API/MostPopularMovies/" + imdbKey;
 
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
@@ -28,10 +28,15 @@ public class App {
         for (int i = 0; i < 3; i++) {
             Map<String, String> filme = listaDeFilmes.get(i);
             // for (Map<String, String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
+            System.out.println("\u001b[1mTítulo:\u001b[m " + filme.get("title"));
+            System.out.println("\u001b[1mURL da Imagem:\u001b[m " + filme.get("image"));
             System.out.println(filme.get("imDbRating"));
-            System.out.println();
+            double classificacao = Double.parseDouble(filme.get("imDbRating"));
+            int numeroEstrelinhas = (int) classificacao;
+            for (int j = 0; j < numeroEstrelinhas; j++) {
+                System.out.print("⭐");
+            }
+            System.out.println("\n");
         }
 
     }
