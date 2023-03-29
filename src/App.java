9897f4d4c3b72daf3e46875b7e442c472e05576f
F1984898 Celso Rodrigues de Freitas Junior <celsojunior@bb.com.br>
2023-03-29 00:06:54 -0300

    Aula 1 - desafio 1

--- src/App.java
+++ src/App.java
@@ -9,9 +9,9 @@ import java.util.Map;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        // fazer uma conexão HTTP e buscar os top 250 filmes
+        // fazer uma conexão HTTP e buscar os top 250 séries
         String imdbKey = System.getenv("IMDB_API_KEY");
-        String url = "https://imdb-api.com/en/API/Top250Movies/" + imdbKey;
+        String url = "https://imdb-api.com/en/API/Top250TVs/" + imdbKey;
 
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
@@ -24,9 +24,14 @@ public class App {
         var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
         // exibir e manipular os dados
-        System.out.println(listaDeFilmes.get(0));
-        for (Map<String, String> filme : listaDeFilmes) {
+        // System.out.println(listaDeFilmes.get(0));
+        for (int i = 0; i < 3; i++) {
+            Map<String, String> filme = listaDeFilmes.get(i);
+            // for (Map<String, String> filme : listaDeFilmes) {
             System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
         }
 
     }
