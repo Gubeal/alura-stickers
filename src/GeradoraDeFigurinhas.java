02432d3932c2923c6352eb2d93330fc65f4fae05
F1984898 Celso Rodrigues de Freitas Junior <celsojunior@bb.com.br>
2023-03-29 03:21:36 -0300

    Aula 2 desafio 2

--- src/GeradoraDeFigurinhas.java
+++ src/GeradoraDeFigurinhas.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
@@ -35,8 +37,14 @@ public class GeradoraDeFigurinhas {
         graphics.setColor(Color.YELLOW);
         graphics.setFont(fonte);
 
-        // escrever uma frase na nova imagem
-        graphics.drawString("TOPZERA", 0, novaAltura - 100);
+        // escrever uma frase na nova imagem]
+        String texto = "TOPZERA";
+        FontMetrics fontMetrics = graphics.getFontMetrics();
+        Rectangle2D retangulo = fontMetrics.getStringBounds(texto, graphics);
+        int larguraTexto = (int) retangulo.getWidth();
+        int posicaoTextoX = (largura - larguraTexto) / 2;
+
+        graphics.drawString(texto, posicaoTextoX, novaAltura - 100);
 
         // escrever a nova imagem em um arquivo
         ImageIO.write(novaImagem, "png", new File(nomeArquivo));
